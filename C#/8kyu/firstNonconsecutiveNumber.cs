30464593a15ff2b4ecb64c77c02458da980a23b3
Jonah Latham <jonah.latham21@gmail.com>
2020-08-08 12:00:00 +0100

    code

---
+++ C#/8kyu/firstNonconsecutiveNumber.cs
@@ -0,0 +1,25 @@
+public class Kata
+{
+  public static object FirstNonConsecutive(int[] arr)
+  {
+      for (int i = 0; i < arr.Length-1; ++i)
+      {
+        if (arr[i] + 1 != arr[i + 1])
+        {
+          return arr[i + 1];
+        }
+      }
+      return null;
+  }
+}
+
+/*
+
+using System.Linq;
+
+public class Kata
+{
+  public static object FirstNonConsecutive(int[] arr) => arr.Cast<int?>().Skip(1).FirstOrDefault(i => i != ++arr[0]);
+}
+
+*/
