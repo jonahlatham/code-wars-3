1081c2c59cc098330b37d83c2a2701fadc2f39a9
Jonah Latham <jonah.latham21@gmail.com>
2020-10-01 12:00:00 +0100

    code

---
+++ C#/8kyu/reverseString.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Text;
+using System.Linq;
+
+public class Kata
+{
+    public static string ReverseWords(string str)
+    {
+        return string.Join(" ", str.Split(' ').Reverse());
+    }
+}
